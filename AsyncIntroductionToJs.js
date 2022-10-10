052ae99b95ac272e30614d66de6d10d53e2c62df
Akatsuki-123 <simantkubde01@gmail.com>
2022-10-10 22:18:20 +0530

    Adding mMy Inf

---
+++ AsyncIntroductionToJs.js
@@ -0,0 +1,13 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name = "Amruta";
+console.log(name);
+let father_name = "Nilkanth Kubde";
+console.log("Father_name");
+let mother_name = "Meena";
+console.log(mother_name);
